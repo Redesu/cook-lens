b57455c6966715c2fc7fde8569f357e9e7f5cc5e
Willyan David <62126803+Redesu@users.noreply.github.com>
2025-10-19 16:19:02 -0300

    Using neon database instead of local

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,12 +1,18 @@
 import { Pool } from "pg";
 
+// if you are using a local database, then use the following:
+// const pool = new Pool({
+//     user: process.env.DB_USER,
+//     host: process.env.DB_HOST,
+//     database: process.env.DB_NAME,
+//     password: process.env.DB_PASSWORD || '',
+//     port: Number(process.env.DB_PORT) || 5432,
+// });
+
 const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD || '',
-    port: Number(process.env.DB_PORT) || 5432,
-});
+    connectionString: process.env.DATABASE_URL,
+    ssl: true
+})
 
 const db = {
     query: (text: string, params?: unknown[]) => pool.query(text, params),
