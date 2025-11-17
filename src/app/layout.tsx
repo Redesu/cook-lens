09fb6f39a53b1db3f26406e44d48f647ead908dd
Willyan David <62126803+Redesu@users.noreply.github.com>
2025-11-16 21:01:47 -0300

    Adds google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
 import { Providers } from "./providers";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -32,6 +33,7 @@ export default function RootLayout({
         <Providers>
           <Header />
           {children}
+          <GoogleAnalytics gaId="G-RQ0RRZ13DP" />
         </Providers>
       </body>
     </html>
