c8c52fa8ab58f9a0a12aaa1dd30a28d4a52197a4
Willyan David <62126803+Redesu@users.noreply.github.com>
2025-12-17 12:08:30 -0300

    Add NODE_ENV to .env.example and update db config
    Added NODE_ENV to the environment example file. Refactored database connection logic in db.ts to use different configurations for production and development environments.

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,21 +1,25 @@
 import { Pool } from "pg";
 
-// if you are using a local database, then use the following:
-// const pool = new Pool({
-//     user: process.env.DB_USER,
-//     host: process.env.DB_HOST,
-//     database: process.env.DB_NAME,
-//     password: process.env.DB_PASSWORD || '',
-//     port: Number(process.env.DB_PORT) || 5432,
-// });
+let pool;
 
-const pool = new Pool({
+if (process.env.NODE_ENV === "production") {
+  pool = new Pool({
     connectionString: process.env.DATABASE_URL,
-    ssl: true
-})
+    ssl: true,
+  });
+} else {
+  pool = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD || "",
+    port: Number(process.env.DB_PORT) || 5432,
+  });
+}
 
 const db = {
-    query: (text: string, params?: unknown[]) => pool.query(text, params),
-}
+  query: (text: string, params?: unknown[]) => pool.query(text, params),
+  end: () => pool.end(),
+};
 
 export default db;
