2b3136b1a7145466db330c95b707d223131ab766
Willyan David <62126803+Redesu@users.noreply.github.com>
2025-12-19 13:18:47 -0300

    Updates variables on db.setup to use the correct id

--- src/tests/db.setup.ts
+++ src/tests/db.setup.ts
@@ -5,29 +5,30 @@ import { test as setup } from "@playwright/test";
 const authFile = "playwright/.auth/user.json";
 
 setup("seed database and authenticate", async ({ page }) => {
-  const result =
-    await db.query(`INSERT INTO users (username, email, created_at) VALUES
-    ('testuser', 'test@test.com', CURRENT_TIMESTAMP)
-    ON CONFLICT (email) DO UPDATE SET username = 'testuser'
-    RETURNING id`);
+  const userResult = await db.query(`
+  INSERT INTO users (username, email, created_at) VALUES
+  ('testuser', 'test@test.com', CURRENT_TIMESTAMP)
+  ON CONFLICT (email) DO UPDATE SET username = 'testuser'
+  RETURNING id
+`);
 
-  const userId = result.rows[0].id;
+  const userId = userResult.rows[0].id;
 
-  await db.query(
+  const recipeResult = await db.query(
     `
-    INSERT INTO recipes (user_id, title, description, ingredients, instructions, prep_time, cook_time, servings, difficulty, created_at) VALUES
-    ($1, 'Test Recipe', 'This is a test recipe.', $2, 'Step 1, Step 2', 10, 20, 2, '⭐', CURRENT_TIMESTAMP)
-    ON CONFLICT DO NOTHING`,
+  INSERT INTO recipes (user_id, title, description, ingredients, instructions, prep_time, cook_time, servings, difficulty, created_at) VALUES
+  ($1, 'Test Recipe', 'This is a test recipe.', $2, 'Step 1, Step 2', 10, 20, 2, '⭐', CURRENT_TIMESTAMP)
+  RETURNING id`,
     [userId, JSON.stringify(["Ingredient 1", "Ingredient 2"])]
   );
 
-  const recipeId = result.rows[0].id;
+  const recipeId = recipeResult.rows[0].id;
 
   await db.query(
     `
-    INSERT INTO saved_recipes (user_id, recipe_id, saved_at) VALUES
-    ($1, $2, CURRENT_TIMESTAMP)
-    ON CONFLICT DO NOTHING`,
+  INSERT INTO saved_recipes (user_id, recipe_id, saved_at) VALUES
+  ($1, $2, CURRENT_TIMESTAMP)
+  ON CONFLICT DO NOTHING`,
     [userId, recipeId]
   );
 
