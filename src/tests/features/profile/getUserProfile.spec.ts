2be350aa020b34b3b24789a01ed976822093ac50
Willyan David <62126803+Redesu@users.noreply.github.com>
2025-12-22 23:32:56 -0300

    Fixes get user profile test

--- src/tests/features/profile/getUserProfile.spec.ts
+++ src/tests/features/profile/getUserProfile.spec.ts
@@ -3,5 +3,5 @@ import { test, expect } from "@playwright/test";
 test("get user profile", async ({ page }) => {
   await page.goto("http://localhost:3000/profile");
   await page.waitForLoadState("networkidle");
-  expect(page.getByRole("heading", { name: "testuser" })).toBeVisible();
+  await expect(page.getByRole("heading", { level: 1, name: "testuser" })).toBeVisible();
 });
